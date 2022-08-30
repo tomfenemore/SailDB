baa14d67d0546884076a838e29bfbcf97fd63e19
tomfenemore <tomfenemore@hotmail.co.uk>
2022-08-30 16:42:25 +0100

    deploy to streamlit cloud

--- main.py
+++ main.py
@@ -2,14 +2,10 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi():
-    # Use a breakpoint in the code line below to debug your script.
-
+import subprocess
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
